5ae5baab427c6d506e089aa7d51a1f787f9ff8eb
Jakub <malinowski.jakub@yahoo.com>
2022-03-09 14:32:03 -0600

    Done with Assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const newHeader = document.createElement('h1');
+document.body.appendChild(newHeader);
+newHeader.id =  'victory';
+newHeader.innerHTML = ('Franisco is the champion');
+
+
+let idk = document.getElementById('main');
+idk.remove();
